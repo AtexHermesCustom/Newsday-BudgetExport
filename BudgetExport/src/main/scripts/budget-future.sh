ff9a0cc9299969740fd88d745d2c8b748a2faff7
jp <jp.mariano@newscycle.com>
2015-08-05 11:46:25 +0800

    rename wrapper scripts

--- BudgetExport/src/main/scripts/budget-future.sh
+++ BudgetExport/src/main/scripts/budget-future.sh
@@ -11,15 +11,25 @@
 INSTALLDIR=`dirname $0`
 test -z "$INSTALLDIR" && INSTALLDIR=.
 
+LOGFILENAME="`basename $0 | cut -d. -f1`.log"
+LOGFILE=$LOGDIR/$LOGFILENAME
+
+echo "`date +'%Y%m%d%H%M%S` START" >> $LOGFILE
+
 # list of publications
 PUBLIST="ND AM"
 
-# date delta
+# date deltas to process
 DELTALIST="3 4 5 6 7 8 9 10 11"
 
 for DELTA in $DELTALIST; do
 	for PUB in $PUBLIST; do
-		echo "Run Budget Export for pub=${PUB}, dateDelta=${DELTA}\n"
-		$INSTALLDIR/run-budget-export.sh -l $PUB -e $DELTA
+		OUTFILE="budget_${PUB}_${DELTA}.xml"
+		echo "*** RUN Budget Export: pub=${PUB}, dateDelta=${DELTA}, outFile=${OUTFILE}" >> $LOGFILE
+		$INSTALLDIR/run-budget-export.sh -l $PUB -e $DELTA -o $OUTFILE >> $LOGFILE 2>&1
 	done
 done
+
+echo "`date +'%Y%m%d%H%M%S` END" >> $LOGFILE
+
+exit
